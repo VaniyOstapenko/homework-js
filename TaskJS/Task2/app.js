dacabffe71a0448cc02394f491498a7ad4190415
VaniyOstapenko <ostapenkovaniykalivan@gmail.com>
2024-06-29 11:57:32 +0300

    homework-js

--- TaskJS/Task2/app.js
+++ TaskJS/Task2/app.js
@@ -4,10 +4,13 @@ const str = 'hello';
 let sogl = 'aeiouy';
 let count = 0;
 
-for (let i = 0; i < str.length; i++) {
-    if (sogl.includes(str[i])) {
-        count++;
+function countNum() {
+    for (let i = 0; i < str.length; i++) {
+        if (sogl.includes(str[i])) {
+            count++;
+        }
     }
 }
 
+countNum()
 console.log(count);
