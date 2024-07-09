bb5f12982ff40d78794d00dba640b58a3940a494
VaniyOstapenko <ostapenkovaniykalivan@gmail.com>
2024-07-09 20:11:16 +0300

    homework-js

--- TaskJS/Task31/app.js
+++ TaskJS/Task31/app.js
@@ -1,8 +1,18 @@
 //Напишите функцию, которая принимает массив строк и возвращает строку, которая встречается чаще всего.
 
 const arr = ['Hi', 'How', 'Are', 'You', 'Hi', 'I"m', 'all', 'right', 'Are', 'Are', 'Are', 'Hi', 'Hi', 'Hi', 'Hi', 'Hi']
-let newArr = arr.filter((el, i) => {
-    return arr.indexOf(el) != i
-})
 
-console.log(newArr);
+let newArr = arr.filter((el, res) => arr.indexOf(el) != res)
+
+let title = ''
+let count = 0;
+
+for (let i = 0; i < newArr.length; i++) {
+    const res = newArr.filter((el) => el == newArr[i])
+    if (res.length > count) {
+        count = res.length
+        title = newArr[i]
+    }
+}
+
+console.log(title);
