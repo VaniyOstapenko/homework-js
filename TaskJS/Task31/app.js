f6f30f99d9ab16d57b33e5d72082609040c45cc3
VaniyOstapenko <ostapenkovaniykalivan@gmail.com>
2024-07-11 18:43:59 +0300

    homework-js

--- TaskJS/Task31/app.js
+++ TaskJS/Task31/app.js
@@ -4,15 +4,15 @@ const arr = ['Hi', 'How', 'Are', 'You', 'Hi', 'I"m', 'all', 'right', 'Are', 'Are
 
 let newArr = arr.filter((el, res) => arr.indexOf(el) != res)
 
-let title = ''
+let name_ = ''
 let count = 0;
 
 for (let i = 0; i < newArr.length; i++) {
     const res = newArr.filter((el) => el == newArr[i])
     if (res.length > count) {
         count = res.length
-        title = newArr[i]
+        name_ = newArr[i]
     }
 }
 
-console.log(title);
+console.log(name_);
